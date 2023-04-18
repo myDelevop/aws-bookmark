569966049c22e22f7232b3b554aff0ccd6003f60
Rocco Caliandro <rocco.caliandro@hotmail.com>
2023-04-18 20:42:38 +0200

    Bug fixing, dev page is ready

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
   <LoginPage msg="New Application in development Branch!"/>
 </template>
+
+<script>
+import LoginPage from './components/LoginPage.vue'
+export default {
+  name: 'App',
+  components: {
+    LoginPage
+  }
+}
+</script>
